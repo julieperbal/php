21a0e5469145fc6765bc30e31622b489da2125ca
julieperbal <julie.perbal@gmail.com>
2017-02-03 14:38:47 +0100

    ajout exercices #2:Les Conditions

---
+++ Conditions/index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+<!-- EXERCICE 1 -->
+<?php
+  // $age = 29;
+  //
+  // if ($age >= 18) {
+  //   echo 'Vous êtes majeur :)';
+  // }
+  // else {
+  //   echo 'Vous êtes mineur :/';
+  // }
+ ?>
+
+<!-- EXERCICE 2 -->
+  <?php
+    // $IsEasy = True;
+    //
+    // if ($IsEasy = true) {
+    //   echo 'C\'est facile!!';
+    // }
+    // else {
+    //   echo 'C\'est difficile !!!!';
+    // }
+   ?>
+
+<!-- EXERCICE 3 -->
+  <?php
+    // $age = 29;
+    // $genre = true;
+    //
+    // if (($genre == true)&&($age>=18)) {
+    //   echo 'Vous êtes un homme et vous êtes majeur';
+    // }
+    // elseif (($genre == true)&&($age<18)) {
+    //   echo 'Vous êtes un homme et vous êtes mineur';
+    // }
+    // elseif (($genre == false)&&($age>=18)) {
+    //   echo 'Vous êtes une femme et vous êtes majeur';
+    // }
+    // else {
+    //   echo 'Vous êtes une femme et vous êtes mineur';
+    // }
+   ?>
+
+<!-- EXERCICE 4 -->
+<!-- <form class="echelle" action="index.php" method="post">
+  Entrez la magnitude du tremblementde terre (entre 1 et 9): <input type="text" name="magnitude">
+  <input type="submit" name="OK">
+</form> -->
+
+  <?php
+  // $magnitude = $_POST['magnitude'];
+  //
+  // switch ($magnitude) {
+  //   case (1):
+  //     echo 'Micro-séisme impossible à ressentir.';
+  //     break;
+  //   case (2):
+  //     echo 'Micro-séisme impossible à ressentir mais enregistrable par les sismomètres.';
+  //     break;
+  //   case (3):
+  //     echo 'Ne cause pas de dégats mais commence à pouvoir être légèrement ressenti.';
+  //     break;
+  //   case (4):
+  //     echo 'Séisme capable de faire bouger des objets mais ne causant généralement pas de dégats.';
+  //     break;
+  //   case (5):
+  //     echo 'Séisme capable d\'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de construction. Peu de dégats sur des bâtiments modernes';
+  //     break;
+  //   case (6):
+  //     echo 'Fort séisme capable d\'engendrer des destructions majeures sur une large distance (180 km) autour de l\'épicentre.';
+  //     break;
+  //   case (7):
+  //     echo 'Séisme capable de destructions majeures à modérées sur une très large zone en fonction de la distance.';
+  //     break;
+  //   case (8):
+  //     echo 'Séisme capable de destructions majeures sur une très large zone de plusieurs centaines de kilomètres.';
+  //     break;
+  //   case (9):
+  //     echo 'Séisme capable de tout détruire sur une très vaste zone.';
+  //     break;
+  // }
+   ?>
+
+<!-- EXERCICE 5 -->
+  <?php
+    // $maVariable = true;
+    //
+    // if ($maVariable == true) {
+    //   echo 'C\'est une développeuse!!!!';
+    // }
+    // else {
+    //   echo 'C\'est un développeur!';
+    // }
+   ?>
+
+<!-- EXERCICE 6 -->
+  <?php
+    // $age = 45;
+    //
+    // if ($age >= 18) {
+    //   echo 'Tu es majeur!!';
+    // }
+    // else {
+    //   echo 'Tu es mineur!';
+    // }
+   ?>
+
+<!-- EXERCICE 7 -->
+  <?php
+    // $maVariable = true;
+    //
+    // if ($maVariable == false) {
+    //   echo 'C\'est pas bon !!';
+    // }
+    // else {
+    //   echo 'C\'est OK !!';
+    // }
+   ?>
+
+<!-- EXERCICE 8 -->
+  <?php
+    // $maVariable = true;
+    //
+    // if ($maVariable == true) {
+    //   echo 'C\'est OK!';
+    // }
+    // else {
+    //   echo 'C\'est pas bon!';
+    // }
+  ?>
+</body>
+</html>
